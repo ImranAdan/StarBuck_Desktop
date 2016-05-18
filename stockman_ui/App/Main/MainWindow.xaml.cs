cc07ca32d7ad853510cde9fc1971ce0d069ef226
Imran Adan <imranadan@googlemail.com>
2016-05-18 20:26:29 +0100

    Added Windsor for IoC, still have issue with loading float values as string from App.config

--- stockman_ui/App/Main/MainWindow.xaml.cs
+++ stockman_ui/App/Main/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+
+using stockman_ui.app.config;
 
 namespace stockman_ui
 {
@@ -20,11 +23,41 @@ namespace stockman_ui
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly float scalingFactor;
+
         public MainWindow()
         {
+            scalingFactor = AppConfigReader.ConfigStringValueAs<long>("mainWindowScaleFactor");
             InitializeComponent();
+            InitialiseMainWindowContainer();
+        }
+
+        private void InitialiseMainWindowContainer()
+        {
+            InitialiseWindowMetrics();
+            InitialiseWindowComponents();
+        }
+
+        private void InitialiseWindowMetrics()
+        {
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth / scalingFactor;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight / scalingFactor;
             this.Left = (System.Windows.SystemParameters.PrimaryScreenWidth / 2) - (this.Width / 2);
             this.Top = (System.Windows.SystemParameters.PrimaryScreenHeight / 2) - (this.Height / 2);
+        }
+
+        private void InitialiseWindowComponents()
+        {
+            WindowTitleBar();
+        }
+
+        private void WindowTitleBar()
+        {
+            var versionNumber = ", v-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var loggedInUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var appLaunchDate = DateTime.Now.ToLongDateString();
+            this.Title += versionNumber + " - " + loggedInUser + " - " + appLaunchDate;
         }
     }
 }
